4d1177e092a66695f4aa2b26fec69607f3b77d67
Lucas Mendes <lucaslsbh1@gmail.com>
2018-09-26 21:30:10 -0300

    [Settings] Mostrando/escondendo conteúdo baseado na primeira visita do usuário

--- src/App.js
+++ src/App.js
@@ -1,35 +1,95 @@
 import React, { Component } from 'react';
 import './App.css'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Logo = styled.div`
   font-size: 1.5em;
 `
 
-const ControlButton = styled.div`
+const ControlButton = styled.a`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+    font-size: 1.2em;
+  `}
+`
 
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+  padding-bottom: 40px;
 `
 
 const AppLayout = styled.div`
   padding: 40px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px;
 `
 
+const Content = styled.div`
+
+`
+
+const checkFirstVisit = () => {
+  const cryptoDashData = localStorage.getItem('cryptoDash')
+  if (!cryptoDashData) {
+    return {
+      firstVisit: true,
+      page: 'settings'
+    }
+  }
+
+  return {}
+}
+
 class App extends Component {
+
+  state = {
+    page: 'dashboard',
+    ...checkFirstVisit()
+  }
+
+  displayingDashboard = () => this.state.page === 'dashboard'
+  displayingSettings  = () => this.state.page === 'settings'
+
+  firstVisitMessage = () => (
+    this.state.firstVisit ? <div>Welcome to CryptoDash, please select your favorite coins to begin.</div> : false
+  )
+
+  confirmFavorites = () => {
+    localStorage.setItem('cryptoDash', 'true')
+    this.setState({ firstVisit: false, page: 'dashboard' })
+  }
+
+  settingsContent = () => (
+    <div>
+      { this.firstVisitMessage() }
+      <div onClick={this.confirmFavorites}>
+        Confirm Favorites
+      </div>
+    </div>
+  )
+
   render() {
-    return (
+    return (  
       <AppLayout>
-        <Logo>
-          CryptoDash
-        </Logo>
-        <div></div>
-        <ControlButton>
-          Dashboard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
+        <Bar>
+          <Logo>
+            CryptoDash
+          </Logo>
+          <div></div>
+
+          {!this.state.firstVisit && (
+            <ControlButton onClick={() => this.setState({page: 'dashboard'})} active={this.displayingDashboard()}>
+              Dashboard
+            </ControlButton>)}
+
+          <ControlButton onClick={() => this.setState({page: 'settings'})} active={this.displayingSettings()}>
+            Settings
+          </ControlButton>
+        </Bar>
+
+        <Content>
+          { this.displayingSettings() && this.settingsContent() }
+        </Content>
       </AppLayout>
     )
   }
